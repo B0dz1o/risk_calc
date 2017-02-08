7e659d7403eeb22bee5eaadd9bfefc6d5d763dc9
Piotr Bogdan <bogdzio@o2.pl>
2017-02-08 03:41:30 +0100

    minor text fix

--- app/src/main/java/com/b0dz1o/riskcalc/BattleParams.java
+++ app/src/main/java/com/b0dz1o/riskcalc/BattleParams.java
@@ -113,27 +113,6 @@ public class BattleParams extends AppCompatActivity {
         return res;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_battle_params, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
